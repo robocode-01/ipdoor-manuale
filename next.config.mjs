6887e44ad985bace85cf29c0b87e2e8db207ab7c
Roberto Portalone <Utente27@ferrero.local>
2025-09-05 15:58:11 +0200

    chore: ignore ESLint during builds on Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 import nextI18NextConfig from './next-i18next.config.js';
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, i18n: nextI18NextConfig.i18n };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // ⬇️ non bloccare i build in presenza di errori ESLint
+    ignoreDuringBuilds: true,
+  },
+  // opzionale: se volessi anche ignorare errori TS nei build
+  // typescript: { ignoreBuildErrors: true },
+  
+  reactStrictMode: true, i18n: nextI18NextConfig.i18n
+};
+
 export default nextConfig;
